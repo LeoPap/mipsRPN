92b4bbc01d43b1746a58d782c2ec57ed16703a98
LeoPap <81252855+LeoPap@users.noreply.github.com>
2023-01-12 19:12:33 +0200

    Add files via upload

--- 3200271_3200031.s
+++ 3200271_3200031.s
@@ -7,12 +7,12 @@
 # STAVROS GIAKOYMIS 3200031 
 # ------------------------------------------------------------------
     .text
-	.globl __start	
+	.globl __start
 
 __start:	
 
-		la $a0,p 			# p = stack
-		li $t2,$zero		# initialize i (i = 0) 
+		
+		li $s1,0		   		# initialize i (i = 0) (pointer)
 		li $v0,4                # print_string syscall code = 4 
 		la	$a0, postfixInput	# load postfix input prompt
 		syscall					# call operating system to perform print operation
@@ -22,16 +22,20 @@ parentLoop:						# Print postfixInput prompt
 		syscall
 		move $t0,$v0			# Store input in $t0
 		beq $t0,eqSign,printResult
-		beq $t0,space,childLoop # if input != ' ' goto childLoop
+		bnq $t0,space,childLoop		# if input != ' ' goto childLoop
+		li $v0, 12 				# get users input 
+		syscall
+		move $t0, $v0			# Store input in $t0
 		li $s4,$zero			# number = 0
-		
+		beq $t0,space,parentLoop# if input != ' ' goto childLoop
+		j childLoop             # go to childLoop
 		
 childLoop:		
 		li $v0, 12 				# get users input 
 		syscall
 		move $t0, $v0			# Store input in $t0
-		beq $t0, space, 		# if input is ' ' goto exitLoop
-		
+		beq $t0, space,parentLoop# if input is ' ' goto parentLoop
+		beq $t0,eqSign,printResult
 		blt $t0, zero,exitLoop	# if ch < 0 goto exitLoop
 		bge $t0, nine,exitLoop  # if ch > 9 goto exitLoop
 		mul $s4, $s4, 10		# number *= 10
@@ -42,10 +46,10 @@ childLoop:
 
 exitLoop:
 		jal calc				# call function calc
-		j parentLoop
+		j parentLoop			# go to parentLoop
 
 printResult:
-		bne $t0, 1,invalidPostfix # if i <> 1 throw invalidPostfix error
+		bne $s1, 1,invalidPostfix # if i <> 1 throw invalidPostfix error
 								  # else
 		li $v0,4                  # print_string syscall code = 4 
 		la	$a0, postfixEvaluation# load postfix evaluation prompt
@@ -54,54 +58,54 @@ printResult:
 		lw $t3,(
 
 pop:	move $v0,$t2
-		beq $t0,$zero,invalidPostfix	# if i == 0 throw invalid error
-		sub $sp,$sp,4					
-		sw $v0, ($sp)
-		addi $s1, $s1, -1 				# i--
-		jr $ra							# return to program
-		
-		   
-push:	beq $t2, MAX, overflowError 	# if i = MAX throw overflow error
-		lb $t1, p($s2)		
-		li $t1, $a0 					# p[i] = result; 						
-		add $sp,$sp,4
-		addi $s2, $s2, 1				# i++
-		jr $ra							# return to program
+			beq $t0,$zero,invalidPostfix	# if i == 0 throw invalid error
+			lw $t4, ($sp)					# pop top
+			add $sp,$sp,4					
+			
+			addi $s1, $s1, -1 				# i--
+			jr $ra							# return to program
+			
+			   
+push:		beq $s1, MAX, overflowError 	# if i = MAX throw overflow error			
+			sub $sp,$sp,4					
+			sw   $a0, 0($s0)				# push a0
+			addi $s1, $s1, 1				# i++
+			jr $ra							# return to program
 
-calc:
+calc:										# Calc function
 		bnq $t0,addSign,subTest 			# if input == '+' 
 				jal pop						# pop()
-				move $s1,$v0				# $s1= $v0
+				move $s2,$v0				# $s1= $v0
 				jal pop						# pop()
-				add $a0,$s1,$v0				# $a0 = $s1 + $v0
+				add $a0,$s2,$v0				# $a0 = $s1 + $v0
 				jal push					# push $a0
 				jr $ra						# return to loop
 				
 		subTest:
 				bnq $t0,minusSign,mulTest 	# if input == '-' 
 				jal pop						# pop()
-				move $s1,$v0				# $s1= $v0
+				move $s2,$v0				# $s1= $v0
 				jal pop						# pop()
-				sub $a0,$s1,$v0				# $a0 = $s1 - $v0
+				sub $a0,$s2,$v0				# $a0 = $s2 - $v0
 				jal push					# push $a0
 				jr $ra						# return to loop
 
 		mulTest:
 				bnq $t0,mulSign,divTest   	# if input == '*' 
 				jal pop						# pop()
-				move $s1,$v0				# $s1= $v0
+				move $s2,$v0				# $s2= $v0
 				jal pop						# pop()
-				mul $a0,$s1,$v0				# $a0 = $s1 * $v0
+				mul $a0,$s2,$v0				# $a0 = $s2 * $v0
 				jal push					# push $a0
 				jr $ra						# return to loop
 
 		divTest:
 				bnq $t0,divSign,divTest   	# if input == '/' 
 				jal pop						# pop()
-				move $s1,$v0				# $s1= $v0
+				move $s2,$v0				# $s2= $v0
 				jal pop						# pop()
 				beq $v0,$zero,divideByZeroError # goto divideByZeroError , print error and exit
-				div $a0,$s1,$v0				# $a0 = $s1 * $v0
+				div $a0,$s2,$v0				# $a0 = $s2 * $v0
 				jal push					# push $a0
 				jr $ra						# return to loop
 				
